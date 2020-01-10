1667074eb8a2afff4556f868ee97b41b87af57aa
rafaelmardegan <rafaelmardegan89@gmail.com>
2020-01-10 17:31:38 -0300

    Iniciando projeto

---
+++ js/index.js
@@ -0,0 +1,131 @@
+
+function check(){
+    if ($(".check").is(':checked')) {
+        $("#pag").css("background-color", "#28a745"); 
+        $("#labelCheck").html("PAGO");
+    }else{
+        $("#pag").css("background-color", "#dc3545"); 
+        $("#labelCheck").html("PENDENTE");
+    } 
+}
+
+$(document).ready(function() {
+	$("#form").hide();
+	$("#mostrarForm").click(function(){
+		$("#form").toggle('fast');
+	});
+	$("#valor").mask('000000000000000.00' , { reverse : true});
+	$("#telefone").mask("(99) 9999-9999?9")
+        .focusout(function (event) {  
+            var target, phone, element;  
+            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+            phone = target.value.replace(/\D/g, '');
+            element = $(target);  
+            element.unmask();  
+            if(phone.length > 10) {  
+                element.mask("(99) 99999-999?9");  
+            } else {  
+                element.mask("(99) 9999-9999?9");  
+            }  
+        });
+
+
+
+    $('#tabela').DataTable( {
+      
+       "bJQueryUI": true,
+       "oLanguage": {
+       		"sProcessing":   "Processando...",
+        	"sLengthMenu":   "Mostrar _MENU_ registros",
+        	"sZeroRecords":  "Não foram encontrados resultados",
+        	"sInfo":         "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+        	"sInfoEmpty":    "Mostrando de 0 até 0 de 0 registros",
+        	"sInfoFiltered": "",
+        	"sInfoPostFix":  "",
+            "sSearch":       "Buscar:",
+            "sUrl":          "",
+            "oPaginate": {
+                "sFirst":    "Primeiro",
+                "sPrevious": "Anterior",
+                "sNext":     "Seguinte",
+                "sLast":     "Último"
+            }
+        },
+        // "ajax": {
+        //     "url": "./teste.txt",
+        //     "dataSrc": "data"
+        // }
+        //    "ajax": {
+        //     "dataType": "json",
+        //     "url": "api.php",
+        //     "data": "data.item"
+        // }
+        
+    });
+
+            $('select').change(function(){
+                $(".btn-pgto").css("color", "white");
+                $(".btn-alterar").css("color", "white");
+            })
+        
+            $(".btn-pgto").css("color", "white");
+            $(".btn-alterar").css("color", "white");
+            
+       
+
+	    $(".salvar").click(function(){
+
+            Swal.fire({
+                title: 'Isso ainda não ta pronto!',
+                width: 480,
+                padding: '3em',
+                background: '#fff url(https://66.media.tumblr.com/31ae8bdb1099aa6a32be3a8aebb100f6/tumblr_pqu6hrmaI91w9murbo1_500.gifv)',
+                backdrop: `
+               
+                top
+                no-repeat`
+            })
+
+    		var nome = $("#nome").val();
+    		var telefone = $("#telefone").val();
+    		var numeroPlaca = $("#numeroPlaca").val();
+    		var dataRetirada = $("#dataRetirada").val();
+    		var valor = $("#valor").val();
+    		var retirado = false;
+    		var dados = {
+    			    data: {nome: nome, telefone: telefone}
+    			};
+
+            Url = "index.php";
+
+            $.post(
+                Url, //Required URL of the page on server
+                dados,
+               function(response,status)
+               {
+                    var obj = jQuery.parseJSON(response);
+                    alert(obj.data.nome);
+                    alert(obj.data.telefone);      
+               }
+            );
+	   })
+     
+            // $("#pag").css("background-color", "green");
+           
+     
+        $(".custom-checkbox").change(function(){
+            check();
+        });
+        check();
+
+
+            
+
+
+
+});
+
+
+// var dados = {
+//                 data: {nome: nome, telefone: telefone}
+//             };
